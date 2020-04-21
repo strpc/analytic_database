d9f3c7bd3d387f317ed48345c22116bd35c91a4e
strpc <renat.kruchinskij@gmail.com>
2020-04-21 14:22:39 +0300

    указан абсолютный путь к папке логов

--- logger.py
+++ logger.py
@@ -5,17 +5,17 @@ import logging.handlers as handlers
 from config import DIR_NAME_LOG, FILE_NAME_LOG, FILESIZE_LOG, COUNT_BACKUP_LOG
 
 
-if not os.path.exists(DIR_NAME_LOG):
+if not os.path.exists(os.path.join(os.getcwd(), DIR_NAME_LOG)):
     try:
-        os.mkdir(DIR_NAME_LOG)
+        os.mkdir(os.path.join(os.getcwd(), DIR_NAME_LOG))
     except Exception as e:
         logger.error('Ошибка при создании папок для логов; {0}'.format(e))
 
 formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
     )
-handler = handlers.RotatingFileHandler(os.path.join(DIR_NAME_LOG, FILE_NAME_LOG), 
+handler = handlers.RotatingFileHandler(os.path.join(DIR_NAME_LOG, FILE_NAME_LOG),
                                        maxBytes=FILESIZE_LOG*1024*1024,
                                        backupCount=COUNT_BACKUP_LOG,
                                        encoding='utf-8')
